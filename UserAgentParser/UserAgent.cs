8037448aa866500900ccfd36b812cd82f93181de
Danny Ng <dannyboyng@gmail.com>
2019-01-18 02:10:28 +0100

    Changed caching strategy to IMemoryCache. Introduce new dependancy on Microsoft.Extensions.Caching.Memory/IMemoryCache 2.2.0

--- UserAgentParser/UserAgent.cs
+++ UserAgentParser/UserAgent.cs
@@ -1,17 +1,39 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.Extensions.Caching.Memory;
+using System;
 
 namespace UserAgentParser
 {
     public class UserAgent
     {
-        private static Dictionary<string, UA> cache = new Dictionary<string, UA>();
+        private static MemoryCacheOptions cacheOptions = new MemoryCacheOptions();
+        private static IMemoryCache cache = new MemoryCache(new MemoryCacheOptions());
+
+        public UserAgent()
+        {
+            cacheOptions.SizeLimit = 10000;
+        }
 
         public static UA Parse(string userAgentString)
         {
-            if (cache.ContainsKey(userAgentString)) return cache[userAgentString];
-            var ua = new UA(userAgentString);
-            cache.Add(userAgentString, ua);
-            return ua;
+            userAgentString = (userAgentString.Length > 512) ? userAgentString.Trim().Substring(0, 512) : userAgentString.Trim();
+            return cache.GetOrCreate(userAgentString, entry =>
+            {
+                entry.SlidingExpiration = TimeSpan.FromDays(3);
+                entry.Size = 1;
+                return new UA(userAgentString);
+            });
         }
+
+        public UA ParseUserAgent(string userAgentString)
+        {
+            userAgentString = (userAgentString.Length > 512) ? userAgentString.Trim().Substring(0, 512) : userAgentString.Trim();
+            return cache.GetOrCreate(userAgentString, entry =>
+            {
+                entry.SlidingExpiration = TimeSpan.FromDays(3);
+                entry.Size = 1;
+                return new UA(userAgentString);
+            });
+        }
+
     }
 }
